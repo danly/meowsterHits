8f57231096b33490ed2b8db475cc73b63e63eff0
danlyg <gih.daniel@gmail.com>
2015-05-25 22:55:22 -0700

    30 randomized stacks created, buttons decrease array

--- views/app.js
+++ views/app.js
@@ -15,16 +15,16 @@ function Game () {
 
 
 	Game.prototype.init = function () {
-
+		var that = this;
 
 		$(this.board.nextButton).click(function (event) {
-			console.log("nextButton was clicked")
+			that.stacks.shuffledStacks = that.board.nextStack(that.stacks.shuffledStacks);
 		})
 
 
 
 		$(this.board.decreaseButton).click(function (event) {
-			console.log("decreaseButton was clicked")
+			that.stacks.shuffledStacks = that.board.decrease(that.stacks.shuffledStacks);
 		})
 
 	}
@@ -38,16 +38,47 @@ function Game () {
 
 function Stacks () {
 	this.currentStack = 0;
-	this.totalStacks = 4;
+	this.totalStacks = 30;
+	this.unshuffledStacks = [0, 0, 0, //3 zeros
+					   1, 1, 1, 1, 1, 1, 1, //7 ones
+					   2, 2, 2, 2, 2, 2, //6 twos
+					   3, 3, 3, 3, 3, //5 threes
+					   4, 4, 4, //3 fours
+					   5, 5, 5, //3 fives
+					   6, 6, 6]; //3 sixes
+
+
+
+	this.shuffledStacks = shuffleArr(this.unshuffledStacks);
 
 }
 
 
 
-	Stacks.prototype.nextStack = function () {
 
 
+
+
+
+
+
+//fisher-yates shuffle
+var shuffleArr = function (arr) {
+	var counter = arr.length;
+	var temp;
+	var index;
+
+	while (counter) {
+		index = Math.floor(Math.random() * counter);
+		counter--;
+		temp = arr[counter];
+		arr[counter] = arr[index];
+		arr[index] = temp;
 	}
+	return arr;
+}
+
+
 
 
 
@@ -59,10 +90,41 @@ function Stacks () {
 function Board () {
 	this.nextButton = $(".next");
 	this.decreaseButton = $(".decrease");
+
 }
 
 
+	Board.prototype.decrease = function (stackInstance) {
+		
+		console.log("decreaseButton was clicked")
+		if(stackInstance[0] !== 0) {
+			stackInstance[0] = stackInstance[0]-1;
+			console.log(stackInstance);
+			return stackInstance;
 
+
+		} else {
+			console.log("YOU LOSE");
+		}
+
+	};
+
+	Board.prototype.nextStack = function (stackInstance) {
+		
+		console.log("nextButton was clicked")
+		if(stackInstance[0] === 0) {
+			stackInstance.shift();
+			console.log(stackInstance);
+			return stackInstance;
+
+
+		} else {
+			console.log("DID NOTHING");
+			
+			console.log(stackInstance);
+			return stackInstance;
+		}
+	};
 
 
 
