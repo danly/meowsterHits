cb8aeafbe33dc66c345f42493f93d7eaae14fdbb
danlyg <gih.daniel@gmail.com>
2015-05-26 17:31:26 -0700

    authenticate working on backend

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require("express"),
 	bodyParser = require("body-parser"),
 	path = require("path");
 
+var db = require("./models");
+
 app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -22,6 +24,30 @@ app.get("/signup", function (req, res) {
 	var signupPath = path.join(views, "signup.html");
 	res.sendFile(signupPath);
 });
+
+app.post("/users", function (req, res) {
+	var newUser = req.body.user;
+
+	db.User.
+	createSecure(newUser, function (err, user) {
+		if (user) {
+			res.send(user);
+		} else {
+			res.redirect("/signup");
+		}
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
 
 app.get("/scores", function (req, res) {
 	var scoresPath = path.join(views, "scores.html");
--- models/user.js
+++ models/user.js
@@ -30,21 +30,40 @@ var confirm = function (password, passwordConfirm) {
 
 userSchema.statics.createSecure = function (params, cb) {
 	var isConfirmed;
-
 	isConfirmed = confirm(params.password, params.password_confirmation);
-
 	if (!isConfirmed) {
 		return cb(console.log("Passwords Should Match"), null);
 	};
-
 	var that = this;
-
 	bcrypt.hash(params.password, 12, function (err, hash) {
 		params.passwordDigest = hash;
 		that.create(params, cb);
 	});
-
 };
+
+userSchema.statics.authenticate = function (params, cb) {
+  	this.findOne({
+      	email: params.email
+    },
+    function (err, user) {
+      	user.checkPswrd(params.password, cb);
+    });
+};
+
+userSchema.methods.checkPswrd = function(password, cb) {
+  	var user = this;
+  	bcrypt.compare(password, 
+ 	this.passwordDigest, function (err, isMatch) {
+    if (isMatch) {
+    	console.log("MATCHED!")
+    	cb(null, user);
+    } else {
+
+    	cb(console.log("OOPS"), null);
+    }
+  });
+};
+
 
 
 var User = mongoose.model("User", userSchema);
