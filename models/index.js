b7afdc3c6e072a2261ba70b5d9d34c1ee270cdaf
danlyg <gih.daniel@gmail.com>
2015-05-28 15:03:04 -0700

    changes

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 var mongoose = require("mongoose");
-mongoose.connect(process.env.MONGOLAB_URI || "mongodb://localhost/smashterHits");
+mongoose.connect(process.env.MONGOLAB_URI ||
+               process.env.MONGOHQ_URL || "mongodb://localhost/smashterHits");
 
 module.exports.User = require("./user");
 module.exports.Scores = require("./scores");
